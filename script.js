924ec000ef07c1c6cffb16c636575ed52018edf6
Dharmik Gohil <86297731+DharmikGohil@users.noreply.github.com>
2023-07-19 16:29:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+window.addEventListener('DOMContentLoaded',function(){
+    var typewritertext = document.getElementById('typewriter');
+    typewritertext.textContent = typewriterext.textContent; 
+});
